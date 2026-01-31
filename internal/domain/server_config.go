c1487bc12c88f78d6a110120de879740b3409f1b
Punisher <palekseev102@gmail.com>
2026-01-31 14:53:57 +0300

    refactor(domain): change string -> time.Duration

--- internal/domain/server_config.go
+++ internal/domain/server_config.go
@@ -1,9 +1,11 @@
 package domain
 
+import "time"
+
 type ServerConfig struct {
-	Address        string `env:"SERVER_ADDRESS,required"`
-	ReadTimeout    string `env:"SERVER_READTIMEOUT,required"`
-	WriteTimeout   string `env:"SERVER_WRITETIMEOUT,required"`
-	IdleTimeout    string `env:"SERVER_IDLETIMEOUT,required"`
-	TimeToShutdown string `env:"SERVER_TIMETOSHUTDOWN,required"`
+	Address        string
+	ReadTimeout    time.Duration
+	WriteTimeout   time.Duration
+	IdleTimeout    time.Duration
+	TimeToShutdown time.Duration
 }

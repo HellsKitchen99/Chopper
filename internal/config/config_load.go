e99c1dcd2bbc3ca4674506acceb56b1e9ec5b2fd
Punisher <palekseev102@gmail.com>
2026-01-31 14:53:20 +0300

    refactor(config): add parse logic string -> time.Duration in ConfigsLoad()

--- internal/config/config_load.go
+++ internal/config/config_load.go
@@ -2,19 +2,41 @@ package config
 
 import (
 	"chopper/internal/domain"
-
-	"github.com/caarlos0/env"
-	"github.com/joho/godotenv"
+	"fmt"
+	"os"
+	"time"
 )
 
 func ConfigsLoad() (domain.ServerConfig, error) {
-	if err := godotenv.Load(); err != nil {
-		return domain.ServerConfig{}, err
-	}
 	var serverConfig domain.ServerConfig
-	if err := env.Parse(&serverConfig); err != nil {
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+	serverReadtimeout := os.Getenv("SERVER_READTIMEOUT")
+	serverWritetimeout := os.Getenv("SERVER_WRITETIMEOUT")
+	serverIdletimeout := os.Getenv("SERVER_IDLETIMEOUT")
+	serverTimeToShutdown := os.Getenv("SERVER_TIMETOSHUTDOWN")
+	if serverAddress == "" || serverReadtimeout == "" || serverWritetimeout == "" || serverIdletimeout == "" || serverTimeToShutdown == "" {
+		return domain.ServerConfig{}, fmt.Errorf("lack of environment variables")
+	}
+	readTimeout, err := time.ParseDuration(serverReadtimeout)
+	if err != nil {
 		return domain.ServerConfig{}, err
 	}
-
+	writeTimeout, err := time.ParseDuration(serverWritetimeout)
+	if err != nil {
+		return domain.ServerConfig{}, err
+	}
+	idleTimeout, err := time.ParseDuration(serverIdletimeout)
+	if err != nil {
+		return domain.ServerConfig{}, err
+	}
+	timeToShutdown, err := time.ParseDuration(serverTimeToShutdown)
+	if err != nil {
+		return domain.ServerConfig{}, err
+	}
+	serverConfig.Address = serverAddress
+	serverConfig.ReadTimeout = readTimeout
+	serverConfig.WriteTimeout = writeTimeout
+	serverConfig.IdleTimeout = idleTimeout
+	serverConfig.TimeToShutdown = timeToShutdown
 	return serverConfig, nil
 }

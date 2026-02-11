2910844a485b7fce21a445d4a242eda106465143
Punisher <palekseev102@gmail.com>
2026-02-11 21:01:44 +0300

    feat(usecase): add custom errors

--- internal/usecase/errors.go
+++ internal/usecase/errors.go
@@ -2,4 +2,11 @@ package usecase
 
 import "errors"
 
+// users
 var ErrUserExists = errors.New("user already exists")
+var ErrUserNotExist = errors.New("user not exist")
+var ErrWrongPassword = errors.New("wrong password")
+
+// notes
+var ErrWrongSleepHourValue = errors.New("wrong sleep hours value")
+var ErrNoteAlreadyExists = errors.New("note already exusts")

203cc2e8c196f691d3014ff443d65690937948e6
Punisher <palekseev102@gmail.com>
2026-02-11 20:57:57 +0300

    feat(usecase): add checking user in database and giving a jwt token and add getting user information from database

--- internal/usecase/user_service.go
+++ internal/usecase/user_service.go
@@ -2,7 +2,8 @@ package usecase
 
 import (
 	"chopper/internal/domain"
-	"chopper/internal/repositoty"
+	"chopper/internal/repository"
+	"chopper/internal/security"
 	"context"
 	"errors"
 	"fmt"
@@ -13,11 +14,13 @@ import (
 
 type UserService struct {
 	userRepository UserRepository
+	jwtService     *security.Jwt
 }
 
-func NewUserService(userRepository UserRepository) *UserService {
+func NewUserService(userRepository UserRepository, jwtService *security.Jwt) *UserService {
 	return &UserService{
 		userRepository: userRepository,
+		jwtService:     jwtService,
 	}
 }
 
@@ -30,10 +33,38 @@ func (u *UserService) CreateUser(ctx context.Context, userRegisterFromFront doma
 		return fmt.Errorf("error while trying to hash password: %v", err)
 	}
 	uuid := uuid.New()
-	if err := u.userRepository.CreateUser(ctx, uuid, username, email, string(passwordHash), defaultRole); err != nil && errors.Is(err, repositoty.ErrUniqueViolation) {
+	if err := u.userRepository.CreateUser(ctx, uuid, username, email, string(passwordHash), defaultRole); err != nil && errors.Is(err, repository.ErrUniqueViolation) {
 		return ErrUserExists
 	} else if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (u *UserService) CheckUserInDatabase(ctx context.Context, userLoginFromFront domain.UserLoginFromFront) (string, error) {
+	username := userLoginFromFront.Username
+	user, err := u.userRepository.CheckUser(ctx, username)
+	if err != nil && errors.Is(err, repository.ErrNoRow) {
+		return "", ErrUserNotExist
+	} else if err != nil {
+		return "", err
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.HashPassword), []byte(userLoginFromFront.Password)); err != nil {
+		return "", ErrWrongPassword
+	}
+	token, err := u.jwtService.GenerateToken(user.Id, user.Username, user.Email, user.Role)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func (u *UserService) GetIdUsernameRole(ctx context.Context, id uuid.UUID, username string) (domain.UserWhoAmI, error) {
+	user, err := u.userRepository.GetIdUsernameRole(ctx, id, username)
+	if err != nil && errors.Is(err, repository.ErrNoRow) {
+		return user, ErrUserNotExist
+	} else if err != nil {
+		return user, err
+	}
+	return user, nil
 }

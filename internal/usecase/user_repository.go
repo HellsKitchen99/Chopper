b96382bec2fda1dd6775d3ac92d073cfce8f9f7c
Punisher <palekseev102@gmail.com>
2026-02-11 21:00:25 +0300

    feat(usecase): add CheckUser and GetIdUsernameRole toUserRepository

--- internal/usecase/user_repository.go
+++ internal/usecase/user_repository.go
@@ -9,4 +9,6 @@ import (
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, uuid uuid.UUID, username, email, hashPassword string, role domain.Role) error
+	CheckUser(ctx context.Context, username string) (domain.User, error)
+	GetIdUsernameRole(ctx context.Context, id uuid.UUID, username string) (domain.UserWhoAmI, error)
 }

1d1baef583123c7e0a208b0a6014d3ef50c03c81
Punisher <palekseev102@gmail.com>
2026-02-12 13:54:58 +0300

    feat(usecase): add some custom errors

--- internal/usecase/errors.go
+++ internal/usecase/errors.go
@@ -8,6 +8,8 @@ var ErrUserNotExist = errors.New("user not exist")
 var ErrWrongPassword = errors.New("wrong password")
 
 // notes
+var ErrWrongMoodValue = errors.New("wrong mood value")
 var ErrWrongSleepHourValue = errors.New("wrong sleep hours value")
+var ErrWrongLoadValue = errors.New("wrong load value")
 var ErrNoteAlreadyExists = errors.New("note already exusts")
 var ErrNoteNotExists = errors.New("note not exists")

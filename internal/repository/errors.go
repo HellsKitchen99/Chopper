ce2e3853d46f28365c4ba5d9fba0fb85b96b2454
Punisher <palekseev102@gmail.com>
2026-02-12 13:20:17 +0300

    feat(repository): add new custom error

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 var ErrUniqueViolation = errors.New("unique violation")
 var ErrNoRow = errors.New("no rows found")
+var ErrDailyEntryNotFound = errors.New("daily entry not found")

4b9403ab3aa18f4621ff1112a5b738543f3ecbb3
HellsKitchen99 <palekseev102@gmail.com>
2026-02-21 13:55:36 +0300

    feat(repository): add migrations to test_database tests entrypoint

--- internal/repository/test_main.go
+++ internal/repository/test_main.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -9,9 +10,14 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/sirupsen/logrus"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 var testPool *pgxpool.Pool
+var migrationsPath = "file://../../migrations" // internal//repository
 
 func TestMain(m *testing.M) {
 	testDataBaseConfig, err := TestDbConfigLoad()
@@ -23,14 +29,29 @@ func TestMain(m *testing.M) {
 	defer cancel()
 	testPool, err = pgxpool.New(ctx, testUrl)
 	if err != nil {
-		testPool.Close()
 		logrus.Fatalf("error while trying to connect to test database: %v", err)
 	}
 	if err := testPool.Ping(ctx); err != nil {
 		testPool.Close()
 		logrus.Fatalf("error while trying to ping test database: %v", err)
 	}
+
+	// добавление миграций
+	mgrt, err := migrate.New(migrationsPath, testUrl)
+	if err != nil {
+		logrus.Fatalf("error whyle trying to create migration object: %v", err)
+	}
+	if err := mgrt.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		mgrt.Close()
+		logrus.Fatalf("error whyle trying to migrated tables: %v", err)
+	}
+
 	resultCode := m.Run()
+	if err := mgrt.Down(); err != nil {
+		mgrt.Close()
+		logrus.Fatalf("error whyle trying to drop database: %v", err)
+	}
+	mgrt.Close()
 	testPool.Close()
 	os.Exit(resultCode)
 }
